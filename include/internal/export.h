1e599aa7a141baf2075f40537ac66c84df1e7313
Elden Armbrust <eldenarmbrust@gmail.com>
2025-11-25 21:37:30 -0700

    fix for linux/mac builds

--- include/internal/export.h
+++ include/internal/export.h
@@ -1,5 +1,12 @@
-#ifdef LIBAEON_EXPORTS
-  #define LIBAEON_API __declspec(dllexport)
+#pragma once
+
+#ifdef _WIN32
+    #ifdef LIBAEON_EXPORTS
+        #define LIBAEON_API __declspec(dllexport)
+    #else
+        #define LIBAEON_API __declspec(dllimport)
+    #endif
 #else
-  #define LIBAEON_API __declspec(dllimport)
+    // On non-Windows platforms, no special export needed
+    #define LIBAEON_API
 #endif
